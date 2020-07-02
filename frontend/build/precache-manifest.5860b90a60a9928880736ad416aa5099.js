d7b8a8361cc2d98e28210e19e8e040387e5433ec
bjorngrape <56294098+bjorngrape@users.noreply.github.com>
2020-07-02 16:39:10 +0200

    build

---
+++ frontend/build/precache-manifest.5860b90a60a9928880736ad416aa5099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a975a039c90d9ae07cd85101e3c4b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be7a3149d8cbaa9b2d7",
+    "url": "/static/css/main.21919026.chunk.css"
+  },
+  {
+    "revision": "ddc1d55baafc66470027",
+    "url": "/static/js/2.faa81158.chunk.js"
+  },
+  {
+    "revision": "24ba9798a6d688083058c754a4aa3bd5",
+    "url": "/static/js/2.faa81158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be7a3149d8cbaa9b2d7",
+    "url": "/static/js/main.df788c9f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
